7a1e5e357905c2a901d26a8606612a0a79bdbbd7
Abel Hurtado Sandoval <83846290+Abelsirius@users.noreply.github.com>
2025-11-02 23:32:14 -0500

    modal

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProductGridComponent } from "../components/product-grid/product-grid";
 import { HeroComponent } from "../components/hero/hero";
 import { HeaderComponent } from "../components/header/header";
 import { TestimonialsComponent } from "../components/testimonials/testimonials";
 import { EncuestasComponent } from "../components/encuesta/encuesta";
 import { Reel } from "../components/reel/reel";
+import { MatDialog } from '@angular/material/dialog';
+import { VotoModalComponent } from '../components/voto-modal-component/voto-modal-component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +14,29 @@ import { Reel } from "../components/reel/reel";
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home  implements OnInit{
+   ngOnInit(): void {
+ this.openVotoModal(); 
+   }
 
+     openVotoModal() {
+       const dialogRef = this.dialog.open(VotoModalComponent, {
+            // Configuración del modal
+            panelClass: 'custom-dialog-container', 
+            backdropClass: 'custom-dialog-backdrop',
+            disableClose: true, // Forzar a usar el botón "INICIAR VOTACIÓN"
+            width: '90%',
+        });
+
+
+        dialogRef.afterClosed().subscribe((e)=>{
+          if(e){
+                const collectionSection = document.getElementById('encuesta');
+    if (collectionSection) {
+      collectionSection.scrollIntoView({ behavior: 'smooth' });
+    }
+          }
+        })
+    }
+   public  dialog = inject(MatDialog);
 }
